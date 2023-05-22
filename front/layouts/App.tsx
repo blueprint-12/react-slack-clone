3592bce3cd9fdb430ac46218683cf1f369edc1e9
blueprint-12 <blueprint12@daum.net>
2023-05-22 22:13:20 +0900

    fix: 코드스플리팅을 위한 suspense fallback추가

--- front/layouts/App.tsx
+++ front/layouts/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 const Workspace = React.lazy(() => import('@layouts/WorkSpace'));
@@ -8,16 +8,18 @@ const SignUp = React.lazy(() => import('@pages/SignUp'));
 const App = () => {
   //어플리케이션이 진입부터 login이 필요함
   return (
-    <Routes>
-      <Route path="/login" element={<LogIn />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/workspace/:workspace" element={<Workspace />} />
+    <Suspense fallback={<>loading...</>}>
+      <Routes>
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/workspace/:workspace" element={<Workspace />} />
 
-      {/* 기본 redirect */}
-      <Route path="/" element={<Navigate to="/login" />} />
-      {/* 없는 페이지 접근 */}
-      <Route path="*" element={<p>404 Not Found</p>} />
-    </Routes>
+        {/* 기본 redirect */}
+        <Route path="/" element={<Navigate to="/login" />} />
+        {/* 없는 페이지 접근 */}
+        <Route path="*" element={<p>404 Not Found</p>} />
+      </Routes>
+    </Suspense>
   );
 };
 
