4e29c0532394b38dfdac5b28e1da9368e3fb55a1
blueprint-12 <blueprint12@daum.net>
2023-06-01 23:36:32 +0900

    feat: Menu 컴포넌트 완료, 이벤트버블링 방지 함수 생성

--- front/components/Menu/index.tsx
+++ front/components/Menu/index.tsx
@@ -1,16 +1,30 @@
 import React, { CSSProperties, ReactNode, useCallback } from 'react';
+import { CloseModalButton, CreateMenu } from './styles';
 
 interface MenuProps {
   children: ReactNode;
   show: boolean;
   onCloseModal: () => void;
   style: CSSProperties;
-  closeButton?: boolean;
 }
-const Menu = ({ children, show, onCloseModal, style, closeButton = true }: MenuProps) => {
+
+// FC<Props> interface Props => react 18 deprecated FC
+const Menu = ({ children, show, onCloseModal, style }: MenuProps) => {
+  // TODO: 자식에서 클릭이벤트 발생시 부모로 이벤트 버블링 막기
+  const stopPropagation = useCallback((e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+  }, []);
+
   if (!show) return null;
 
-  return <div>{children}</div>;
+  return (
+    <CreateMenu onClick={onCloseModal}>
+      <div onClick={stopPropagation} style={style}>
+        {show && <CloseModalButton onClick={onCloseModal}>&times;</CloseModalButton>}
+        {children}
+      </div>
+    </CreateMenu>
+  );
 };
 
 export default Menu;
