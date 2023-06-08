66eef9c4104984ec3329ba3243a0be60925ca6b5
blueprint-12 <blueprint12@daum.net>
2023-06-09 00:07:55 +0900

    feat: ChatList, ChatBox 하위 컴포넌트 배치 및 props 끌어올리기

--- front/pages/Channel/index.tsx
+++ front/pages/Channel/index.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { FormEvent, useCallback } from 'react';
 import { Container, Header } from './styles';
+import ChatList from '@components/ChatList';
+import ChatBox from '@components/ChatBox';
+import useInput from '@hooks/useInput';
 
 const Channel = () => {
+  const [chat, onChangeChat] = useInput('');
+  const onSubmitForm = useCallback((e: FormEvent) => {
+    e.preventDefault();
+  }, []);
   return (
     <Container>
       <Header>채널 헤더</Header>
+      <ChatList />
+      <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} />
     </Container>
   );
 };
--- front/pages/DirectMessage/index.tsx
+++ front/pages/DirectMessage/index.tsx
@@ -1,10 +1,13 @@
 import { Container } from '@pages/Channel/styles';
 import fetcher from '@utils/fetcher';
-import React from 'react';
+import React, { FormEvent, useCallback } from 'react';
 import useSWR from 'swr';
 import { useParams } from 'react-router';
 import gravatar from 'gravatar';
 import { Header } from './styles';
+import ChatBox from '@components/ChatBox';
+import ChatList from '@components/ChatList';
+import useInput from '@hooks/useInput';
 
 const DirectMessage = () => {
   const { workspace, id } = useParams();
@@ -12,22 +15,27 @@ const DirectMessage = () => {
   //TODO: 워크스페이스 멤버 데이터 가져오기
   //? GET/workspaces/:workspace/members/:id
   const { data: userData } = useSWR(`/api/users`, fetcher);
-  const { data: memberData } = useSWR(`/api/workspaces/${workspace}/members/${id}`, fetcher);
-  console.log('memberData', memberData);
-  //TODO: 유저데이터(나)
-
+  const { data: memberData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
+  const [chat, onChangeChat, setChat] = useInput('');
   // 값이 없으면 로딩중이거나 에러(SWR 2버전에서는 두개를 구분하는 state 제공)
-  if (!userData) return null;
-  if (!memberData) return <div>loading...</div>;
+
+  const onSubmitForm = useCallback((e: FormEvent) => {
+    e.preventDefault();
+    console.log('submit');
+    setChat('');
+  }, []);
+
+  if (!userData || !memberData) return null;
 
   return (
     <Container>
       <Header>
         <img src={gravatar.url(memberData.email, { s: '25px', d: 'retro' })} alt={memberData.nickname} />
+        <span>{memberData.nickname}</span>
       </Header>
+      <ChatList />
+      <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} />
     </Container>
-    // <ChatLists/>
-    // <ChatBox/>
   );
 };
 
